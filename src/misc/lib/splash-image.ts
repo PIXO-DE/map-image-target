d0d0dd8d51b5d4014e29e5627d25cbf16b60db84
unknown <rambudiarto@gmail.com>
2025-05-04 21:23:34 +0700

    add splash screen

---
+++ src/misc/lib/splash-image.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+//@ts-nocheck
+
+const splashImageComponent = {
+    schema: {
+      disableWorldTracking: {type: 'bool', default: false},
+      requestGyro: {type: 'bool', default: false},
+    },
+    init() {
+      const splashimage = document.getElementById('splashimage');
+      splashimage.style.display = 'block';
+      const start = document.getElementById('start');
+      start.style.display = 'block';
+  
+      const addXRWeb = () => {
+        if (this.data.requestGyro === true && this.data.disableWorldTracking === true) {
+          // If world tracking is disabled, and you still want gyro enabled (i.e. 3DoF mode)
+          // Request motion and orientation sensor via XR8's permission API
+          XR8.addCameraPipelineModule({
+            name: 'request-gyro',
+            requiredPermissions: () => ([XR8.XrPermissions.permissions().DEVICE_ORIENTATION]),
+          });
+        }
+        this.el.sceneEl.setAttribute('xrweb', `allowedDevices: any; disableWorldTracking: ${this.data.disableWorldTracking}`);
+        splashimage.classList.add('hidden');
+        start.classList.add('hidden');
+      };
+  
+      setTimeout(() => {
+        start.click(); // Trigger click event on the start button after 3 seconds
+      }, 3000);
+      
+      start.onclick = addXRWeb;
+    },
+  };
+  
+  export { splashImageComponent };
--- src/pages/ar/App.tsx
+++ src/pages/ar/App.tsx
@@ -1,6 +1,7 @@
 import { playVideoComponent } from "../../misc/lib/target";
 import { AFrameScene } from "../../templates/aframe.template";
 import html from "../../misc/scene/index.html?raw";
+import { splashImageComponent } from "../../misc/lib/splash-image";
 
 const AR = () => {
   return (
@@ -9,6 +10,7 @@ const AR = () => {
         sceneHtml={html}
         components={[
           { name: "play-video", val: playVideoComponent },
+          { name: "splash-image", val: splashImageComponent },
         ]}
       />
     </>
